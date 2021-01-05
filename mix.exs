3a8127c243670a3db47d755f6c422403925a08f6
ryo.kato.a.ts <lyo.kato@gmail.com>
2021-01-05 19:39:43 +0000

    v0.1.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Requiem.MixProject do
   def project do
     [
       app: :requiem,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.11",
       package: package(),
       compilers: [:rustler] ++ Mix.compilers(),
