30c2fd6dad371ccd2bb87294d697563eba77a9ae
ryo.kato.a.ts <lyo.kato@gmail.com>
2021-09-24 08:26:39 +0000

    v0.3.8

--- README.md
+++ README.md
@@ -30,7 +30,7 @@ by adding `requiem` to your list of dependencies in `mix.exs`:
 ```elixir
 def deps do
   [
-    {:requiem, "~> 0.3.7"}
+    {:requiem, "~> 0.3.8"}
   ]
 end
 ```
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Requiem.MixProject do
   def project do
     [
       app: :requiem,
-      version: "0.3.7",
+      version: "0.3.8",
       elixir: "~> 1.11",
       package: package(),
       start_permanent: Mix.env() == :prod,
